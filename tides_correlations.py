0dff56342ea798bcf63ce99163c2feba81217d28
arosoga <Adrian-Rosoga@users.noreply.github.com>
2020-02-13 21:16:22 +0000

    Find time shift for which correlation is max

--- tides_correlations.py
+++ tides_correlations.py
@@ -1,9 +1,30 @@
+import datetime
 from tides import tide_data_generator_from_web, parse, TIDE_INFO_WEBPAGE_TEMPLATE, STATIONS
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def correlation(station_1, station_2):
+def shifted_view(df1, dfx, delta_mins, plot = False):
+
+    df2 = dfx.copy()
+
+    for ix in df2.index:
+        df2['Date'][ix] += pd.Timedelta(minutes=delta_mins)
+
+    df_all = pd.merge(df1, df2, how='inner', on='Date')
+
+    if plot:
+        df_all.plot(x='Date', y=['Level_x', 'Level_y'], kind='line')
+        plt.show()
+
+    correlation = df_all.corr()['Level_x']['Level_y'] * 100
+
+    #print(f'After shifting by {delta_mins} minutes correlation = {correlation}%')
+
+    return correlation
+
+
+def process(station_1, station_2):
 
     dfs = []
 
@@ -20,19 +41,38 @@ def correlation(station_1, station_2):
 
     df_all.plot(x='Date', y=['Level_x', 'Level_y'], kind='line')
 
-    correlation = df_all.corr()
+    correlation = df_all.corr()['Level_x']['Level_y'] * 100.0
 
-    print(correlation)
+    print(f'Before {correlation:.2f}%')
+
+    shift2correlation = dict()
+
+    for delta_mins in range(- 15 * 4 * 6, 15 * 4 * 6, 15):
+        correlation = shifted_view(dfs[0], dfs[1], delta_mins)
+
+        shift2correlation[correlation] = delta_mins
+
+        print(f'{delta_mins} ---> {correlation:.1f}%')
+
+    #shifted_view(dfs[0], dfs[1], -210, plot=True)
+    #shifted_view(dfs[0], dfs[1], 165, plot=True)
+
+    max_correlation = max(shift2correlation.keys())
+    delta_mins_max_correlation = shift2correlation[max_correlation]
+
+    print(f'Max correlation {max_correlation:.1f} for delta mins {delta_mins_max_correlation}')
+    print(f'{delta_mins_max_correlation // 60}h {delta_mins_max_correlation % 60}m')
+
+    shifted_view(dfs[0], dfs[1], delta_mins_max_correlation, plot=True)
 
     plt.show()
-
-    return
 
 
 def main():
 
-    correlation('Chelsea', 'Dover')
-    #correlation('Chelsea', 'Westminster')
+    #process('Chelsea', 'Dover')
+
+    process('Chelsea', 'Westminster')
 
 
 if __name__ == '__main__':
