f5a63fa20c10dcb3a856931b84c8c6a55bcb5f0d
arosoga <Adrian-Rosoga@users.noreply.github.com>
2020-02-20 06:34:08 +0000

    Correct time between peaks

--- tides_correlations.py
+++ tides_correlations.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from tides import tide_data_generator_from_web, parse, TIDE_INFO_WEBPAGE_TEMPLATE, STATIONS
 
 
-def shifted_view(df1, df2, delta_mins, plot=False):
+def get_correlation_with_shift(df1, df2, delta_mins, plot=False, message=None):
 
     df2_shifted = df2.copy()
 
@@ -20,7 +20,11 @@ def shifted_view(df1, df2, delta_mins, plot=False):
     df_all = df_all.merge(df2, how='inner', on='Date')
 
     if plot:
-        df_all.plot(x='Date', y=[df_all.columns[1], df_all.columns[2], df_all.columns[3]], title='Tide Level', figsize=(15, 10), kind='line')
+        title = 'Tide Level'
+        if message is not None:
+            title += '\n' + message
+        df_all.plot(x='Date', y=[df_all.columns[1], df_all.columns[2], df_all.columns[3]], title=title,
+                    grid=True, legend=True, figsize=(15, 10), kind='line')
         plt.show()
 
     correlation = df_all.corr()[df_all.columns[1]][df_all.columns[2]] * 100
@@ -43,32 +47,58 @@ def process(station1, station2):
 
     correlation = df_all.corr()[station1][station2] * 100.0
 
-    print(f'Correlation {correlation:.2f}%')
-
     shift2correlation = dict()
 
+    # Magic numbers here - iterate the range plus/minus 6 hours with a 15 mins step
     for delta_mins in range(- 15 * 4 * 6, 15 * 4 * 6, 15):
-        correlation = shifted_view(dfs[0], dfs[1], delta_mins)
-
-        shift2correlation[correlation] = delta_mins
-
-        print(f'{delta_mins} ---> {correlation:.1f}%')
+        correlation_with_shift = get_correlation_with_shift(dfs[0], dfs[1], delta_mins)
+        shift2correlation[correlation_with_shift] = delta_mins
+        print(f'{delta_mins:3} mins ---> {correlation_with_shift:.1f}% correlation')
 
     max_correlation = max(shift2correlation.keys())
     delta_mins_max_correlation = shift2correlation[max_correlation]
 
-    print(f'Max correlation {max_correlation:.1f}% for shift of {delta_mins_max_correlation // 60}h {delta_mins_max_correlation % 60}m')
+    time_delta = f'{abs(delta_mins_max_correlation) // 60}h{abs(delta_mins_max_correlation) % 60}m'
+    time_delta = f'-{time_delta}' if delta_mins_max_correlation < 0 else time_delta
 
-    shifted_view(dfs[0], dfs[1], delta_mins_max_correlation, plot=True)
+    print(f'Correlation {correlation:.2f}%')
+    print(f'Max correlation {max_correlation:.1f}% for time shift of {time_delta}')
+
+    message = f'{time_delta} between peak tides at {station1} and {station2}'
+
+    get_correlation_with_shift(dfs[0], dfs[1], delta_mins_max_correlation, plot=True, message=message)
 
 
 def main():
 
-    process('Chelsea', 'Dover')
+    parser = argparse.ArgumentParser(description='Find time between high tides at two stations.\nDefault stations are Chelsea and Dover.')
+    parser.add_argument('--list', help='list all stations', action='store_true')
+    parser.add_argument('--station1', help='station 1')
+    parser.add_argument('--station2', help='station 2')
+    parser.add_argument('--noplot', help='do not show the plot on screen', action='store_true')
+    parser.add_argument('--save', help='save to file', action='store_true')
+    args = parser.parse_args()
 
-    # process('Chelsea', 'Westminster')
+    if args.list:
+        for station in STATIONS.keys():
+            print(station)
+        sys.exit(0)
 
-    # process('Chelsea', 'Tower Pier')
+    show_plot = False if args.noplot else True
+    save_to_file = False if not args.save else True
+
+    station1 = args.station1
+    station2 = args.station2
+
+    if (station1 is None and station2 is not None) or (station1 is not None and station2 is None):
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    if station1 is None and station2 is None:
+        station1 = 'Chelsea'
+        station2 = 'Dover'
+
+    process(station1, station2)
 
 
 if __name__ == '__main__':
